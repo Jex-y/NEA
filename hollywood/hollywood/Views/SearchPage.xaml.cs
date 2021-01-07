74d298f956dc56b17ee4fe8beff7480c3c801aaf
Edward Jex <edwardjex@live.co.uk>
2021-01-07 00:57:07 +0000

    Added search bar functionality with its own page and API calls.

---
+++ hollywood/hollywood/Views/SearchPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace hollywood.ViewModels
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SearchPage : ContentPage
+    {
+        readonly SearchPageViewModel vm;
+        public SearchPage()
+        {
+            InitializeComponent();
+            BindingContext = vm = new SearchPageViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            vm.SearchCommand.Execute("");
+        }
+    }
+}
