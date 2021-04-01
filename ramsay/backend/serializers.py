cf78e57557186856c6427a96fe96c514ace5deef
Edward Jex <edwardjex@live.co.uk>
2021-04-01 22:52:16 +0100

    Added documentation

--- ramsay/backend/serializers.py
+++ ramsay/backend/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from .models import *
 
+"""
+Serializer obejcts are used to convert a model object into json formated data.
+This is used by the REST API to display data returned from an API call.
+"""
+
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
